b3447fd111d1425fb45dcd98febb3e460e877764
Gerard <g.roura.v@protonmail.com>
2022-06-21 20:58:46 +0100

    sprint 7, exercici 5

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,83 +1,24 @@
 import styled from 'styled-components';
 
-export const WrapperWebOptions = styled.div`
+export const WrapperApp = styled.div`
   padding: 1em;
-  border: 2px solid black;
+  box-shadow: 0px 0px 12px rgba(69, 69, 69, 0.5);
   border-radius: 1em;
   width: fit-content;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 6em;
+  text-align: center;
+  padding-bottom: 1.5em;
 `;
 
-export const WrapperInput = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const LabelInput = styled.label`
-  width: 10em;
-`;
-
-export const InputNumber = styled.input`
-  width: 2em;
-`;
-
-export const AddButton = styled.button`
-  position: relative;
-  display: inline-block;
-  width: 1.6em;
-  height: 1.6em;
-  border-radius: 0.3em;
+export const Btn = styled.button`
   border: 1px solid black;
-  background-color: orange;
-  cursor: pointer;
-  margin-left: 0.2em;
-  margin-right: 0.2em;
-
-  &::before {
-    content: '';
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translateX(-50%) translateY(-50%);
-    width: 0.7em;
-    height: 0.2em;
-    background-color: white;
-    border-radius: 0.5em;
-  }
-  &::after {
-    content: '';
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translateX(-50%) translateY(-50%);
-    width: 0.2em;
-    height: 0.7em;
-    background-color: white;
-    border-radius: 0.5em;
-  }
+  background-color: white;
+  border-radius: 2em;
+  padding: 0.3em 0.6em;
 `;
 
-export const SubstractButton = styled.button`
-  position: relative;
-  display: inline-block;
-  width: 1.6em;
-  height: 1.6em;
-  border-radius: 0.3em;
-  border: 1px solid black;
-  background-color: orange;
-  cursor: pointer;
-  margin-left: 0.2em;
-  margin-right: 0.2em;
-
-  &::before {
-    content: '';
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translateX(-50%) translateY(-50%);
-    width: 0.7em;
-    height: 0.2em;
-    background-color: white;
-    border-radius: 0.5em;
-  }
+export const Ancor = styled.a`
+  text-decoration: none;
 `;
---
+++ src/Budget.js
@@ -0,0 +1,262 @@
+import { useEffect, useReducer } from 'react';
+import {
+  WrapperWebOptions,
+  WrapperInput,
+  LabelInput,
+  InputNumber,
+  AddButton,
+  SubstractButton,
+} from './Budget.styles';
+
+function Budget() {
+  const { budgetForm, dispatchBudgetForm } = useBudgetForm();
+
+  if (!budgetForm) return null;
+  return (
+    <>
+      <p>Que vols fer?</p>
+      <div>
+        <input
+          type="checkbox"
+          id="web"
+          checked={budgetForm.web.active}
+          onChange={() =>
+            dispatchBudgetForm({
+              type: 'web_changed',
+              value: !budgetForm.web.active,
+            })
+          }
+        />
+        <label htmlFor="web">Una pàgina web (500e)</label>
+      </div>
+      {budgetForm.web.active && (
+        <WrapperWebOptions>
+          <WrapperInput>
+            <LabelInput>Número de pàgines</LabelInput>
+            <AddButton
+              onClick={() =>
+                dispatchBudgetForm({
+                  type: 'number_pages_changed',
+                  value: budgetForm.pages.numberPages + 1,
+                })
+              }
+            />
+            <InputNumber
+              value={budgetForm.pages.numberPages}
+              onChange={(e) =>
+                dispatchBudgetForm({
+                  type: 'number_pages_changed',
+                  value: e.target.value,
+                })
+              }
+            ></InputNumber>
+            <SubstractButton
+              onClick={() =>
+                dispatchBudgetForm({
+                  type: 'number_pages_changed',
+                  value: budgetForm.pages.numberPages - 1,
+                })
+              }
+            />
+          </WrapperInput>
+          <WrapperInput>
+            <LabelInput>Número de llenguatges</LabelInput>
+            <AddButton
+              onClick={() =>
+                dispatchBudgetForm({
+                  type: 'number_languages_changed',
+                  value: budgetForm.pages.numberLanguages + 1,
+                })
+              }
+            />
+            <InputNumber
+              value={budgetForm.pages.numberLanguages}
+              onChange={(e) =>
+                dispatchBudgetForm({
+                  type: 'number_languages_changed',
+                  value: e.target.value,
+                })
+              }
+            ></InputNumber>
+            <SubstractButton
+              onClick={() =>
+                dispatchBudgetForm({
+                  type: 'number_languages_changed',
+                  value: budgetForm.pages.numberLanguages - 1,
+                })
+              }
+            />
+          </WrapperInput>
+        </WrapperWebOptions>
+      )}
+      <div>
+        <input
+          type="checkbox"
+          id="seo"
+          checked={budgetForm.seo.active}
+          onChange={() =>
+            dispatchBudgetForm({
+              type: 'seo_changed',
+              value: !budgetForm.seo.active,
+            })
+          }
+        />
+
+        <label htmlFor="seo">Una consultoria seo (300e)</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          id="googleAds"
+          checked={budgetForm.googleAds.active}
+          onChange={() =>
+            dispatchBudgetForm({
+              type: 'google_ads_changed',
+              value: !budgetForm.googleAds.active,
+            })
+          }
+        />
+        <label htmlFor="googleAds">Una campanya de google ads (200e)</label>
+      </div>
+      <p>Preu: {budgetForm.totalPrice}&#8364;</p>
+    </>
+  );
+}
+
+const budgetFormReducer = (state, action) => {
+  switch (action.type) {
+    case 'web_changed': {
+      if (!action.value) {
+        return {
+          ...state,
+          web: {
+            active: !state.web.active,
+            price: state.web.price,
+          },
+          pages: {
+            numberPages: 0,
+            numberLanguages: 0,
+          },
+          totalPrice:
+            state.totalPrice -
+            state.web.price -
+            state.pages.numberPages * 30 -
+            state.pages.numberLanguages * 30,
+        };
+      }
+      return {
+        ...state,
+        web: {
+          active: !state.web.active,
+          price: state.web.price,
+        },
+        totalPrice: state.totalPrice + state.web.price,
+      };
+    }
+
+    case 'seo_changed': {
+      return {
+        ...state,
+        seo: {
+          active: !state.seo.active,
+          price: state.seo.price,
+        },
+        totalPrice: state.seo.active
+          ? state.totalPrice - state.seo.price
+          : state.totalPrice + state.seo.price,
+      };
+    }
+
+    case 'google_ads_changed':
+      return {
+        ...state,
+        googleAds: {
+          active: !state.googleAds.active,
+          price: state.googleAds.price,
+        },
+        totalPrice: state.googleAds.active
+          ? state.totalPrice - state.googleAds.price
+          : state.totalPrice + state.googleAds.price,
+      };
+
+    case 'number_pages_changed': {
+      if (action.value < 0 || isNaN(action.value)) return { ...state };
+      return {
+        ...state,
+        pages: {
+          numberPages: action.value,
+          numberLanguages: state.pages.numberLanguages,
+        },
+        totalPrice:
+          state.totalPrice - state.pages.numberPages * 30 + action.value * 30,
+      };
+    }
+
+    case 'number_languages_changed': {
+      if (action.value < 0 || isNaN(action.value)) return { ...state };
+      return {
+        ...state,
+        pages: {
+          numberPages: state.pages.numberPages,
+          numberLanguages: action.value,
+        },
+        totalPrice:
+          state.totalPrice -
+          state.pages.numberLanguages * 30 +
+          action.value * 30,
+      };
+    }
+
+    case 'all':
+      return {
+        ...action.value,
+      };
+
+    default:
+      throw new Error('Error type not recognized');
+  }
+};
+
+const INITIAL_BUDGET_FORM_VALUES = {
+  web: {
+    active: false,
+    price: 500,
+  },
+  seo: {
+    active: false,
+    price: 300,
+  },
+  googleAds: {
+    active: false,
+    price: 200,
+  },
+  pages: {
+    numberPages: 0,
+    numberLanguages: 0,
+  },
+  totalPrice: 0,
+};
+const useBudgetForm = () => {
+  const [budgetForm, dispatchBudgetForm] = useReducer(budgetFormReducer, null);
+
+  useEffect(() => {
+    if (budgetForm)
+      localStorage.setItem('budgetFormValues', JSON.stringify(budgetForm));
+  }, [budgetForm]);
+
+  useEffect(() => {
+    const budgetFormValues = JSON.parse(
+      localStorage.getItem('budgetFormValues')
+    );
+    if (!budgetFormValues)
+      dispatchBudgetForm({ type: 'all', value: INITIAL_BUDGET_FORM_VALUES });
+    if (budgetFormValues)
+      dispatchBudgetForm({ type: 'all', value: { ...budgetFormValues } });
+  }, []);
+
+  return {
+    budgetForm,
+    dispatchBudgetForm,
+  };
+};
+export default Budget;
